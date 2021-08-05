07d124f8f164e8695c6f3a0420cc6f1ada35db30
MobinX <47663360+MobinX@users.noreply.github.com>
2021-08-05 18:39:55 +0600

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { Link, Route, Switch } from 'react-router-dom'
+
+// Auto generates routes from files under ./pages
+// https://vitejs.dev/guide/features.html#glob-import
+import { Link, Route, Switch } from 'react-router-dom'
+
+// Auto generates routes from files under ./pages
+// https://vitejs.dev/guide/features.html#glob-import
+// const pages = import.meta.globEager('./pages/*.jsx')
+
+// const routes = Object.keys(pages).map((path) => {
+//   const name = path.match(/\.\/pages\/(.*)\.jsx$/)[1]
+//   return {
+//     name,
+//     path: name === 'Home' ? '/' : `/${name.toLowerCase()}`,
+//     component: pages[path].default
+//   }
+// })
+
+
+
+export function App() {
+  return (
+    <>
+      <div> Hello World</div>
+    </>
+  )
+}
