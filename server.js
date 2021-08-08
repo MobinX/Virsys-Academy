6c2857a829a09e0c519dc474632db158695357d3
MobinX <mobin0219@gmail.com>
2021-08-08 17:40:25 +0000

    added conf

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ async function createServer(
   const app = express()
   
 
-  const mongoDB = 'mongodb://127.0.0.1/my_database';
-  mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+  var config = require('./config/' + 'dev' + '.config')
+  mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true });
   
   const db = mongoose.connection;
-  
+
   //Bind connection to error event (to get notification of connection errors)
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
