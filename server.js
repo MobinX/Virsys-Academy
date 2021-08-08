a2df226d9369477a1ce76059799f5c00d0dde4e2
MobinX <mobin0219@gmail.com>
2021-08-08 11:41:37 +0000

    change

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 
-//Firts Test 
-
 const isTest = process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD
 
 async function createServer(
