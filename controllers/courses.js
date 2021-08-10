3fe9ff3244d2cd9ba22b546008fdace156ffa9e6
MobinX <mobin0219@gmail.com>
2021-08-10 09:33:18 +0000

    bb

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,7 +2,58 @@ const courseModel = require("../models/course");
 const contentModel = require("../models/content");
 const userModel = require("../models/user");
 
+exports.getcourseList = (req,res) => {
+
+}
+
+exports.getcoursebyid = (req,res) => {
+
+}
+
+
+exports.add = (req,res) => {
+
+}
+
 exports.courseList = (req,res) => {
 
 }
+
+exports.courseList = (req,res) => {
+
+}
+
+exports.courseList = (req,res) => {
+
+}
+
+exports.courseList = (req,res) => {
+
+}
+
+exports.courseList = (req,res) => {
+
+}
+
+exports.courseList = (req,res) => {
+
+}
+
+exports.courseList = (req,res) => {
+
+}
+
+exports.courseList = (req,res) => {
+
+}
+
+exports.courseList = (req,res) => {
+
+}
+
+exports.courseList = (req,res) => {
+
+}
+
+
 
