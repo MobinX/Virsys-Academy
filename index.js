8d30c04ad32da3302dbd87490118b081fdf34126
MobinX <47663360+MobinX@users.noreply.github.com>
2021-08-17 00:29:23 +0600

    updt

--- index.js
+++ index.js
@@ -12,62 +12,62 @@ const bodyParser = require('body-parser')
 const isTest = process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD
 
 async function createServer(
-  root = process.cwd(),
-  isProd = process.env.NODE_ENV === 'production'
-) {
+//   root = process.cwd(),
+//   isProd = process.env.NODE_ENV === 'production'
+// ) {
 
-  const resolve = (p) => path.resolve(__dirname, p)
+//   const resolve = (p) => path.resolve(__dirname, p)
 
-  const indexProd = isProd
-    ? fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
-    : ''
+//   const indexProd = isProd
+//     ? fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
+//     : ''
 
-  const app = express()
+//   const app = express()
   
 
-  var config = require('./configs/' + 'dev' + '.config')
-  mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true });
+//   var config = require('./configs/' + 'dev' + '.config')
+//   mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true });
   
-  const db = mongoose.connection;
+//   const db = mongoose.connection;
 
-  //Bind connection to error event (to get notification of connection errors)
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//   //Bind connection to error event (to get notification of connection errors)
+//   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
-  /**
-   * @type {import('vite').ViteDevServer}
-   */
-  let vite
-  if (!isProd) {
-    vite = await require('vite').createServer({
-      root,
-      logLevel: isTest ? 'error' : 'info',
-      server: {
-        middlewareMode: 'ssr',
-        watch: {
-          // During tests we edit the files too fast and sometimes chokidar
-          // misses change events, so enforce polling for consistency
-          usePolling: true,
-          interval: 100
-        }
-      }
-    })
-    // use vite's connect instance as middleware
-    app.use(vite.middlewares)
-    app.use(bodyParser.json())
-  } else {
-    app.use(require('compression')())
-    app.use(
-      require('serve-static')(resolve('dist/client'), {
-        index: false
-      })
-    )
-  }
+//   /**
+//    * @type {import('vite').ViteDevServer}
+//    */
+//   let vite
+//   if (!isProd) {
+//     vite = await require('vite').createServer({
+//       root,
+//       logLevel: isTest ? 'error' : 'info',
+//       server: {
+//         middlewareMode: 'ssr',
+//         watch: {
+//           // During tests we edit the files too fast and sometimes chokidar
+//           // misses change events, so enforce polling for consistency
+//           usePolling: true,
+//           interval: 100
+//         }
+//       }
+//     })
+//     // use vite's connect instance as middleware
+//     app.use(vite.middlewares)
+//     app.use(bodyParser.json())
+//   } else {
+//     app.use(require('compression')())
+//     app.use(
+//       require('serve-static')(resolve('dist/client'), {
+//         index: false
+//       })
+//     )
+//   }
 
   
 
-//   await require('./routes/index')(app);
-  app.use(cors())
+// //   await require('./routes/index')(app);
+//   app.use(cors())
   
 
 //   app.use('*', async (req, res) => {
@@ -108,7 +108,7 @@ async function createServer(
       res.send("hello");
     })
 
-  return { app, vite }
+  return { app, {} }
 }
 
 
