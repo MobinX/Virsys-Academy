f96cdcf67ca4b4212b7a4e4bc95e6faa6fe44a50
MobinX <47663360+MobinX@users.noreply.github.com>
2021-08-17 20:28:49 +0600

    Update move-dist.sh

--- scripts/move-dist.sh
+++ scripts/move-dist.sh
@@ -5,7 +5,7 @@ echo "Running"
 
 mkdir public
 cp -r dist/client public
-mkdir api/renderer
+
 mv dist/client/ssr-manifest.json api/renderer/
 cp dist/server/entry-server.js api/renderer/
 cp dist/client/index.html api/renderer/
