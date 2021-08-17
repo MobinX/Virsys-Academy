813271e71439b1962abfb74d77f2b65d01ed9d58
MobinX <47663360+MobinX@users.noreply.github.com>
2021-08-17 19:58:15 +0600

    Update move-dist.sh

--- scripts/move-dist.sh
+++ scripts/move-dist.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 mkdir public
 cp -r dist/client public
 mkdir api/renderer
-mv public/ssr-manifest.json api/renderer/
+mv dist/client/ssr-manifest.json api/renderer/
 cp dist/server/entry-server.js api/renderer/
 cp dist/client/index.html api/renderer/
