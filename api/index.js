0f9021e7388fb0f56657c9004b8972978dcc30ba
MobinX <47663360+MobinX@users.noreply.github.com>
2021-08-17 16:40:50 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -120,4 +120,16 @@ createServer().then(({ app }) =>
   }))
 
 
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
 
