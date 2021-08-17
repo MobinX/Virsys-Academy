662403ff7a970525a3de45f9d2a8aa8efb24cb91
MobinX <47663360+MobinX@users.noreply.github.com>
2021-08-17 20:36:10 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const log = require("./renderer/exm)
+log("ki")
+
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
