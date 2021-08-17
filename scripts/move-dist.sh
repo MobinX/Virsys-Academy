f2dfda98d96e40bbb93788aa88e52f6ee356b242
MobinX <47663360+MobinX@users.noreply.github.com>
2021-08-17 18:25:07 +0600

    Create move-dist.sh

---
+++ scripts/move-dist.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir public
+cp -r dist/client public
+mkdir api/renderer
+mv public/ssr-manifest.json api/renderer/
+cp dist/server/entry-server.js api/renderer/
+cp dist/client/index.html api/renderer/
